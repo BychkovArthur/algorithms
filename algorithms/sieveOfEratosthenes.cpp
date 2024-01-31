c8913b5056c3f91e6f0712aa7b7e68a2edcf2dd0
Артур <bychkov.arthur@gmail.com>
2024-01-31 21:55:56 +0300

    add sieve of Eratosthenes

---
+++ algorithms/sieveOfEratosthenes.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+constexpr size_t maxN = 121;
+
+void sieve(std::bitset<maxN + 1>& bs, size_t n) {
+    bs[0] = false;
+    bs[1] = false;
+
+    for (size_t prime = 2; prime * prime <= n; ++prime) {
+        if (!bs[prime]) {
+            continue;
+        }
+
+        for (size_t composite = prime * prime; composite <= n; composite += prime) {
+            bs[composite] = false;
+        }
+    }
+}
+
+int main() {
+    size_t n;
+    std::cin >> n;
+
+    std::bitset<maxN + 1> bs;
+    bs.set();
+
+    sieve(bs, n);
+
+    for (size_t i = 0; i <= maxN; ++i) {
+        std::cout << i << ": " << bs[i] << std::endl;
+    }
+}
